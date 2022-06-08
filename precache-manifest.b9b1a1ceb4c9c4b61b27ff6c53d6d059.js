f6e696212f8a235745529029171e9747208a6aaf
Moginder <mogiegan@gmail.com>
2022-06-08 23:39:59 +0530

    Updates

---
+++ precache-manifest.b9b1a1ceb4c9c4b61b27ff6c53d6d059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89de6c0c8f928f6c92bb0a9e500d9545",
+    "url": "/daily-info/index.html"
+  },
+  {
+    "revision": "7bdea6a4d1679263b53d",
+    "url": "/daily-info/static/css/2.2a52c1dd.chunk.css"
+  },
+  {
+    "revision": "04f9c122b7506293270d",
+    "url": "/daily-info/static/css/main.67fae926.chunk.css"
+  },
+  {
+    "revision": "7bdea6a4d1679263b53d",
+    "url": "/daily-info/static/js/2.49f5bde0.chunk.js"
+  },
+  {
+    "revision": "eac39ed27381daa977a96bfff84c33cc",
+    "url": "/daily-info/static/js/2.49f5bde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f9c122b7506293270d",
+    "url": "/daily-info/static/js/main.19fbf911.chunk.js"
+  },
+  {
+    "revision": "bc209c5b603b24dda5bd",
+    "url": "/daily-info/static/js/runtime-main.45f169c8.js"
+  },
+  {
+    "revision": "c972eed32217bf33e03a581dfa44dd43",
+    "url": "/daily-info/static/media/profile-pic.c972eed3.jpg"
+  }
+]);
